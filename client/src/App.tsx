84733e93e1bfe3a08ab4710da22d6794d747c351
Konrad Nowak <shaderpl@gmail.com>
2022-05-19 14:36:15 +0200

    Add axios fetching daya from backend, add flashcard interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Flashcard } from "./flashcard.js";
+
 
 function App() {
+  const [flashcards, setFlashcards] = useState<Flashcard[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/flashcards").then(response => {
+      setFlashcards(response.data);
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +21,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <ol>
+          {flashcards.map(flashcard =>
+            <li key={flashcard.id}>{flashcard.title} - {flashcard.content}</li>
+          )}
+        </ol>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ client/src/flashcard.ts
@@ -0,0 +1,7 @@
+export interface Flashcard {
+    id:          string;
+    title:       string;
+    date:        Date;
+    description: string;
+    content:     string;
+}
