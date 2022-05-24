1ea591eaf2b5a14573a6b1a3c27d7eb028bf699e
Konrad Nowak <shaderpl@gmail.com>
2022-05-24 21:46:15 +0200

    Add base template from mui templates

---
+++ client/src/darkTheme.tsx
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+export const darkTheme = createTheme({
+    palette: {
+        primary: {
+          main: '#373737',
+        },
+        secondary: {
+          main: '#f50057',
+        },
+        background: {
+          default: '#82828a',
+        },
+      },
+});
