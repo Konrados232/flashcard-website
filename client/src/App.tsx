3cb4462e30e6c7f23ba96f4bdb8948c6596272b6
Konrad Nowak <shaderpl@gmail.com>
2022-05-25 13:05:00 +0200

    Add MainGrid component as the base look

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBar from './NavBar';
-
+import MainGrid from './MainGrid';
 
 function App() {
     return (
         <div className="App">
             <NavBar />
+
+            <MainGrid />
+            
         </div>
     );
 }
